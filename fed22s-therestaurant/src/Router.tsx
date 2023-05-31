f8c40cfd16ea9429851143df19fec33008fc502e
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-05-31 14:01:49 +0200

    feat - added component files for path elements in router

--- fed22s-therestaurant/src/Router.tsx
+++ fed22s-therestaurant/src/Router.tsx
@@ -1,28 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
-
+import { Home } from "./components/Home";
+import { Booking } from "./components/Booking";
+import { Contact } from "./components/Contact";
+import { Admin } from "./components/Admin";
 
 export const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
         path: "/",
-        element: <Layout></Layout>,
-        children: [
-            {
-                path:"/",
-                index: true
-            },
-            {
-                path: "/booking",
-                element:
-            },
-            {
-                path:"/contact",
-                element:
-            },
-            {
-                path:"/admin",
-                element:
-            }
-        ]
-    }
+        element: <Home></Home>,
+        index: true,
+      },
+      {
+        path: "/booking",
+        element: <Booking></Booking>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/admin",
+        element: <Admin></Admin>,
+      },
+    ],
+  },
 ]);
