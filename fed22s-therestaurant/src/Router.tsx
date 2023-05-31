9e3f5d1860850fddfb77a573fd410dbc5483285b
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-05-31 13:40:33 +0200

    feat - add routes

---
+++ fed22s-therestaurant/src/Router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/home",
+        element:
+    },
+    {
+        path:"/booking",
+        element:
+    },
+    {
+        path:"/contact",
+        element:
+    },
+    {
+        path: "/admin",
+        element:
+    }
+]);
