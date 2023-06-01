92ffb5b40a3473e2b9a3a0583299edee1537c7ef
Marcus Reineck <100363230+MarcusRei@users.noreply.github.com>
2023-06-01 20:49:59 +0200

    🎨 make seedDb more clear and add more mockData

--- fed22s-backend/seedDB/mockdata/customer.js
+++ fed22s-backend/seedDB/mockdata/customer.js
@@ -1,7 +1,22 @@
-exports.customer = [
+exports.mockCustomers = [
   {
     name: "Junita",
     email: "junita@gmail.com",
     phonenumber: "+46735456064",
+  },
+  {
+    name: "Marcus",
+    email: "marcus@gmail.com",
+    phonenumber: "0760505645",
+  },
+  {
+    name: "Fannie",
+    email: "fannie@gmail.com",
+    phonenumber: "+46734857864",
+  },
+  {
+    name: "Jocke",
+    email: "jocke@gmail.com",
+    phonenumber: "+46715665067",
   },
 ];
--- fed22s-backend/seedDB/seedDB.js
+++ fed22s-backend/seedDB/seedDB.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 const Booking = require("../src/models/Booking");
 const Customer = require("../src/models/Customer");
-const { bookings } = require("./bookings");
-const { customer } = require("./mockdata/customer");
+const { mockBookings } = require("./bookings");
+const { mockCustomers } = require("./mockdata/customer");
 
 const populateDbWithMockData = async (MONGO_CONNECTION_STRING) => {
   let conn;
@@ -14,12 +14,11 @@ const populateDbWithMockData = async (MONGO_CONNECTION_STRING) => {
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
 
-    // POPULATE DATA ACCOORDING TO YOUR MODELS
     await Booking.deleteMany();
-    await Customer.create(customer);
-    await Booking.create(bookings);
+    await Customer.create(mockCustomers);
+    await Booking.create(mockBookings);
 
-    console.log("Database successfully populated with test data");
+    console.log("Database successfully populated with mockdata");
   } catch (error) {
     console.error(error);
   } finally {
