9eee19714b226ff8f965ef63d0bb2056dc3cfb33
Marcus Reineck <100363230+MarcusRei@users.noreply.github.com>
2023-06-05 12:49:45 +0200

    ✨ update booking form

--- fed22s-therestaurant/src/components/Booking.tsx
+++ fed22s-therestaurant/src/components/Booking.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import { Form } from "./Form";
 import { ImageBackground } from "./styled/ImageBackground";
+import { BookingConfirmation } from "./BookingConfirmation";
+import { BookingContext } from "../contexts/BookingContext";
 
 export const Booking = () => {
+  /* const [booking, setBooking] = useState<Booking>({
+    name: "",
+    email: "",
+    phonenumber: "",
+    guests: 0,
+    date: "",
+    time: "",
+  }); */
+
   return (
     <>
+      {/* <BookingContext.Provider value={booking}> */}
+      {/* {confirm ? <BookingConfirmation></BookingConfirmation> : } */}
       <Form></Form>
+      {/* </BookingContext.Provider> */}
     </>
   );
 };
--- fed22s-therestaurant/src/components/Form.tsx
+++ fed22s-therestaurant/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
 import { Customer } from "../models/Customer";
 import { Input } from "./styled/Input";
 import { StyledForm } from "./styled/StyledForm";
@@ -7,28 +7,40 @@ import { LeftsideDiv } from "./styled/LeftsideDiv";
 import { Heading } from "./styled/Heading";
 import { Button } from "./styled/Button";
 import { ImageBackground } from "./styled/ImageBackground";
+import { Booking } from "../models/Booking";
+import { BookingContext } from "../contexts/BookingContext";
 
 export const Form = () => {
   const [policyChecked, setPolicyChecked] = useState(false);
-  const [customer, setCustomer] = useState<Customer>({
+  const [dateChosen, setDateChosen] = useState(false);
+  const [confirm, setConfirm] = useState(false);
+
+  const [booking, setBooking] = useState<Booking>({
     name: "",
     email: "",
-    phone: "",
+    phonenumber: "",
     guests: 0,
+    date: "",
+    time: "",
   });
+
+  /* const booking = useContext(BookingContext); */
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    console.log(customer);
+
+    setConfirm(true);
+
+    console.log("Form sent!");
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const prop = e.target.name;
     if (e.target.type === "text") {
-      setCustomer({ ...customer, [prop]: e.target.value });
+      setBooking({ ...booking, [prop]: e.target.value });
     }
     if (e.target.type === "number") {
-      setCustomer({ ...customer, [prop]: +e.target.value });
+      setBooking({ ...booking, [prop]: +e.target.value });
     }
   };
 
@@ -36,66 +48,82 @@ export const Form = () => {
     setPolicyChecked(e.target.checked);
   };
 
+  const openCalendar = (e: FormEvent) => {
+    e.preventDefault();
+    setBooking({ ...booking, date: "05-06-2023", time: "18:00-21:00" });
+    setDateChosen(true);
+
+    console.log("date and time set!");
+  };
+
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <Heading>Boka bord</Heading>
-      <label>
-        Namn:
-        <Input
-          type="text"
-          value={customer.name}
-          name="name"
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Email:
-        <Input
-          type="text"
-          value={customer.email}
-          name="email"
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Telefonnummer:
-        <Input
-          type="text"
-          value={customer.phone}
-          name="phone"
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <LeftsideDiv>
+    <>
+      <StyledForm>
+        <Heading>Boka bord</Heading>
         <label>
-          Antal i sällskap:
-          <NumberInput
-            type="number"
-            min={1}
-            max={10}
-            value={customer.guests}
-            name="guests"
+          Namn:
+          <Input
+            type="text"
+            value={booking.name}
+            name="name"
             onChange={handleChange}
             required
           />
         </label>
-        <Button>Välj tid</Button>
-      </LeftsideDiv>
-      <label>
-        <input type="checkbox" onChange={handleCheckbox} />
-      </label>
-      <p>
-        Genom att boka bord på Le Restaurante godkänner jag att mina uppgifter
-        sparas enligt denna
-        <a href="https://book.easytablebooking.com/book/privacy/?id=b6e01&lang=SE">
-          {" "}
-          integritetspolicy
-        </a>
-      </p>
-      <Button disabled={!policyChecked}>Boka</Button>
-    </StyledForm>
+        <label>
+          Email:
+          <Input
+            type="text"
+            value={booking.email}
+            name="email"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Telefonnummer:
+          <Input
+            type="text"
+            value={booking.phonenumber}
+            name="phonenumber"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <LeftsideDiv>
+          <label>
+            Antal i sällskap:
+            <NumberInput
+              type="number"
+              min={1}
+              max={10}
+              value={booking.guests}
+              name="guests"
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <Button onClick={openCalendar}>Välj tid</Button>
+        </LeftsideDiv>
+        <label>
+          <input type="checkbox" onChange={handleCheckbox} />
+        </label>
+        <p>
+          Genom att boka bord på Le Restaurante godkänner jag att mina uppgifter
+          sparas enligt denna
+          <a href="https://book.easytablebooking.com/book/privacy/?id=b6e01&lang=SE">
+            {" "}
+            integritetspolicy
+          </a>
+        </p>
+        <Button
+          disabled={!policyChecked}
+          onClick={dateChosen ? handleSubmit : undefined}
+        >
+          Boka
+        </Button>
+      </StyledForm>
+      {confirm && <h2>Din bokning är registrerad!</h2>}
+    </>
   );
 };
