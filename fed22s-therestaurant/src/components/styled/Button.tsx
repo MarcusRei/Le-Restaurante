f9e651f8b511736817835009888e9dc337a259d8
Junita <junita.berglin@gmail.com>
2023-06-07 22:12:47 +0200

    💄(style): hover buttons

--- fed22s-therestaurant/src/components/styled/Button.tsx
+++ fed22s-therestaurant/src/components/styled/Button.tsx
@@ -19,12 +19,19 @@ export const CallUsButton = styled(Button)`
     border: 1.5px solid lightgray;
     width: 12em;
   }
+  &:hover {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
 `;
 export const BookingButton = styled(Button)`
   border: 1px solid lightgray;
   background-color: white;
+
   @media (min-width: 844px) {
     border: 1.5px solid lightgray;
     width: 14em;
+  }
+  &:hover {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   }
 `;
