6dc6c13a22d10bca556e8c3de6cb248b8df2aaa9
Junita <junita.berglin@gmail.com>
2023-06-07 13:44:42 +0200

    💄(style): add symbols

--- fed22s-therestaurant/src/components/Footer.tsx
+++ fed22s-therestaurant/src/components/Footer.tsx
@@ -1,5 +1,15 @@
 import { StyledFooter } from "./styled/StyledFooter";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon icon={faInstagram} />
+    </StyledFooter>
+  );
 };
