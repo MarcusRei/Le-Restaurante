03acedbdcf42342184bcb110362d8fa83d3d7d4e
Junita <junita.berglin@gmail.com>
2023-06-12 15:31:14 +0200

    🔥 mockdata

--- fed22s-backend/seedDB/bookings.js
+++ fed22s-backend/seedDB/bookings.js
@@ -3,33 +3,18 @@ exports.mockBookings = [
     date: "2023-09-04",
     time: "18.00-21.00",
     guests: 4,
-    customer: {
-      _id: "647750c6c00de3f00e764371",
-      name: "Marcus",
-      email: "marcus@gmail.com",
-      phonenumber: "0760505645",
-    },
+    customer: "6478a5324116fa0d45aca2f4",
   },
   {
     date: "2023-09-05",
     time: "18.00-21.00",
     guests: 6,
-    customer: {
-      _id: "647750c6c00de3f00e764371",
-      name: "Fannie",
-      email: "fannie@gmail.com",
-      phonenumber: "+46734857864",
-    },
+    customer: "647750c6c00de3f00e764372",
   },
   {
     date: "2023-09-06",
     time: "21.00-23.00",
     guests: 8,
-    customer: {
-      _id: "647750c6c00de3f00e764371",
-      name: "Jocke",
-      email: "jocke@gmail.com",
-      phonenumber: "+46715665067",
-    },
+    customer: "647750c6c00de3f00e764373",
   },
 ];
