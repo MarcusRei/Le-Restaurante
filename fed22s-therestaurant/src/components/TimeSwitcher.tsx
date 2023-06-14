c8f9db8960b1c3b37de8d6e54c14dd67ffa3a0f2
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-06-14 13:33:47 +0200

    feat implement start of timeswitch in admin page

--- fed22s-therestaurant/src/components/TimeSwitcher.tsx
+++ fed22s-therestaurant/src/components/TimeSwitcher.tsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import { BookingHeading } from "./styled/BookingHeading";
 import { TimeSwitchSlider, TimeSwitchWrapper } from "./styled/TimeSwitchStyled";
 
 export const TimeSwitcher = () => {
-  let switcher = false;
+  let [switcher, setSwitcher] = useState(false);
+
+  const handleSwitch = () => {
+    setSwitcher(!switcher);
+  };
+
   console.log(switcher);
   return (
     <TimeSwitchWrapper>
-      <TimeSwitchSlider
-        switcher={switcher}
-        onClick={() => {
-          switcher = !switcher;
-          console.log(switcher);
-        }}
-      >
-        18:00
+      <TimeSwitchSlider switcher={switcher} onClick={handleSwitch}>
+        {" "}
+        {switcher ? <p>18:00</p> : <p>21:00</p>}
       </TimeSwitchSlider>
     </TimeSwitchWrapper>
   );
